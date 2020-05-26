591e6d51c2d912ded6f6596f399a1f2a4e0c72c7
simsid66 <simsid66@gmail.com>
2020-05-26 23:22:27 +0530

    finalised UI, added grid drawing routine; about to start painting code

--- main.js
+++ main.js
@@ -1,38 +1,89 @@
+let win, canvas, ctx;
+let mouse = {
+    x: 0,
+    y: 0
+}
+let drawing_area_bounds;
+
 function init() {
     nw.Screen.Init();
-    let win = nw.Window.get();
-    let canvas = document.getElementById('canvas');
+    win = nw.Window.get();
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
     win.setAlwaysOnTop(true);
-
-/*     var screenCB = {
-    onDisplayBoundsChanged: function(screen) {
-        console.log('displayBoundsChanged', screen);
-    },
-
-    onDisplayAdded: function(screen) {
-        console.log('displayAdded', screen);
-    },
-
-    onDisplayRemoved: function(screen) {
-        console.log('displayRemoved', screen)
-    }
-    };
-
-    nw.Screen.on('displayBoundsChanged', screenCB.onDisplayBoundsChanged);
-    nw.Screen.on('displayAdded', screenCB.onDisplayAdded);
-    nw.Screen.on('displayRemoved', screenCB.onDisplayRemoved); */
-
-    new_win.on('focus', function() {
-        console.log('New window is focused');
-    });
-
 
     let work_area = nw.Screen.screens[0].work_area;
 
-    win.resizeTo(work_area.width, work_area.height * 0.20);
+    win.resizeTo(Math.round(work_area.width), Math.round(work_area.height * 0.30));
     win.moveTo(work_area.x, work_area.height - win.height);
 
-    ctx = canvas.getContext('2d');
+    canvas.style.width = 'calc(100% - 60px)';
+    canvas.style.height = 'calc(100% - 60px)';
+
+    win.setResizable(false);
 }
 
+function draw_grid() {
+    const text_height = 60;
+    const text_width = 12;
+    const start_x = (canvas.width % text_width) / 2;
+    const end_x = canvas.width - start_x;
+    const lower_y = ((canvas.height - text_height) / 2) + text_height;
+    const upper_y = lower_y - text_height;
+    drawing_area_bounds = {
+        0: {
+            x: start_x,
+            y: upper_y
+        },
+
+        1: {
+            x: end_x,
+            y: upper_y
+        },
+
+        2: {
+            x: start_x,
+            y: lower_y
+        },
+
+        3: {
+            x: end_x,
+            y: lower_y
+        }
+    }
+
+    ctx.strokeStyle = 'rgba(200, 200, 200, 0.6)';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+
+    ctx.moveTo(start_x, lower_y);
+    ctx.lineTo(end_x, lower_y);
+
+    ctx.moveTo(start_x, upper_y);
+    ctx.lineTo(end_x, upper_y);
+
+    for (let x = start_x + 0.5; x <= end_x; x += text_width) {
+        ctx.moveTo(x, lower_y);
+        ctx.lineTo(x, lower_y - text_height);
+    }
+
+    ctx.stroke();
+}
+
+function assignPageEvtListeners() {
+    canvas.addEventListener('mousemove', function (e) {
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+    }, false);
+
+    canvas.addEventListener('mousedown', function () {
+
+    })
+}
+
+
+
 init();
+draw_grid();
+assignPageEvtListeners();
